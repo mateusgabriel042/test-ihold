05ab6900b839be59e48b54f858cc490dead325ac
mateusgabriel042 <mateus.gabriel042@gmail.com>
2023-08-17 22:32:50 -0300

    change details in seeders

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -15,7 +15,7 @@ class CustomerSeeder extends Seeder
     public function run(): void
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('products_status')->truncate();
+        DB::table('customers')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         $customer = [
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            OrderStatusSeeder::class,
             ProductStatusSeeder::class,
+            OrderStatusSeeder::class,
             RolesSeeder::class,
             CustomerSeeder::class
         ]);
